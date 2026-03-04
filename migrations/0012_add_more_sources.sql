af5625914592695427299eb55ee3828d7742fcca
alexraskin <alexraskin@fastmail.fm>
2026-03-04 13:12:23 +0100

    add more sources

---
+++ migrations/0012_add_more_sources.sql
@@ -0,0 +1,7 @@
+INSERT INTO sources (id, name, url, type, active) VALUES
+  ('src-iran-news-daily', 'Iran News Daily', 'https://irannewsdaily.com/feed/', 'rss', 1),
+  ('src-iran-herald', 'Iran Herald', 'http://feeds.iranherald.com/rss/1b76a2b4cf7810bd', 'rss', 1),
+  ('src-nyt-iran', 'New York Times Iran', 'https://www.nytimes.com/svc/collections/v1/publish/https://www.nytimes.com/topic/destination/iran/rss.xml', 'rss', 1),
+  ('src-iran-times', 'Iran Times', 'https://iran-times.com/feed/', 'rss', 1),
+  ('src-iran-primer', 'The Iran Primer (USIP)', 'https://iranprimer.usip.org/rss.xml', 'rss', 1),
+  ('src-ifp-news', 'Iran Front Page', 'https://ifpnews.com/feed/', 'rss', 1);
